4418afefd990d59e2af7f95a661cf4d8990243a9
Isaac Bythewood <isaac@bythewood.me>
2013-03-21 00:12:43 -0300

    Fix sudoer add and nebula host errors

--- server-init.sh
+++ server-init.sh
@@ -47,7 +47,7 @@ cd /home/isaac
 mkdir .ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAy5XMcfPGgYptBZrInhhpAnUbAa1e+uqgaT90c5KeqkMMKdejxBlxSU11eKcK0Ns3FCivbl/QB319sOHMqB6JV3HgSd/sg+zIPSRZA+iaufZ9Bkj3lkkWI4kWO9PttRqnuWvdIewPK+1X3UUQQeVHf7Cv++kEUDfFYnxfQDFJ13pcvy8Ja6aINdbPQ6MIXVUKsGLIL2hQyFhpX8RFQbsQYcwBwa+kWYCCzRiqj9ijvyTAZJ0OujT9Xj3ccNHJj7eUhwKSjzlHHq4uBKquQByXAenFTTsmAhu3u/2f5yJmRbIqInu6/DrNbJydpRUL3ah+XpyaN2EvudoBAfUSlRv6lXOyrumfZJEs+EIoshMEVHuv4ldlgnkQjuyF6MoFGg7AjnnG53IRDOWQrXPB+F2wMV5mxx2j31uEENwc/pkP2sHLYKsOLLIajSZjv1y4PyKt8xvlPSzGs/r6T0+tM16SV8kQShj+gDZvXeVBvTI97XQUzHDIN/Lr//QTeFOmtVFr4Ol6258/B8vaSiyjU80w8BXqFreoTtYqwnETlU3P7lM5XghunfPxveUUYDtADz/0vnA5VGmleenGuFoI91KuTC0N1n3doN/KJLIXAKf8gR7Blw5DtMfLv6T/fSuM9TuoEMfHXYuTLySCNrD7dt8VlajnvElDy1vI/Utg7Pu7X+k= isaac@bythewood.me" > .ssh/authorized_keys
 chown -R isaac:isaac /home/isaac
-sed -i 's/root    ALL=(ALL:ALL) ALL/isaac   ALL=(ALL:ALL) ALL/g' /etc/sudoers
+echo "isaac ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 
 echo "================================================================================"
@@ -101,6 +101,7 @@ echo "==========================================================================
 echo "Set hostname..."
 echo "================================================================================"
 echo "nebula" > /etc/hostname
+echo "127.0.0.1 nebula" >> /etc/hosts
 
 
 echo "================================================================================"
