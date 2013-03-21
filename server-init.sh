6bede987b9af1a1d54a76e2003c7ee0fc64ef9c6
Isaac Bythewood <isaac@bythewood.me>
2013-03-20 23:37:01 -0300

    Setup hostname and more tips for passwd setting

--- server-init.sh
+++ server-init.sh
@@ -96,7 +96,15 @@ APT::Periodic::Download-Upgradeable-Packages \"1\";
 APT::Periodic::AutocleanInterval \"7\";
 APT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/10periodic
 
+
+echo "================================================================================"
+echo "Set hostname..."
+echo "================================================================================"
+echo "nebula" > /etc/hostname
+
+
 echo "================================================================================"
 echo "DONE - Be sure you switch to the new user 'isaac' using the password 'changeme'"
-echo "and set a strong password."
+echo "and set a strong password. The quickest way is to run the following immediately:"
+echo "    passwd isaac"
 echo "================================================================================"
