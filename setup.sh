da8c46bf5acc83a77043df4c90f14542990e6f4f
Isaac Bythewood <isaac@bythewood.me>
2014-03-06 03:14:56 -0500

    Build-dep python-lxml addition

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ echo "========================================================================="
 apt-get --yes install fail2ban ufw vim tmux nginx lftp python-virtualenv git \
                       unattended-upgrades htop pwgen build-essential \
                       libssl-dev zlib1g-dev e2fslibs-dev weechat
-apt-get --yes build-dep python-imaging python-psycopg2
+apt-get --yes build-dep python-imaging python-psycopg2 python-lxml
 mkdir -p /root/code
 cd /root/code
 
