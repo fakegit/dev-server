0d506ed57d6091cd10ab42ef19379b1ba1136070
Isaac Bythewood <isaac@bythewood.me>
2016-07-07 16:01:37 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,8 +30,7 @@ echo "Installing applications..."
 echo "========================================================================="
 apt-get --yes install fail2ban ufw vim tmux nginx python-virtualenv git htop \
                       unattended-upgrades pwgen build-essential docker.io \
-                      postgresql imagemagick libav-tools mosh iftop openvpn \
-                      easy-rsa
+                      postgresql imagemagick libav-tools mosh iftop redis-server
 apt-get --yes build-dep python-imaging python-psycopg2 python-lxml
 mkdir -p /root/code
 cd /root/code
